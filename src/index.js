e98343d828b1e44bfa223a8ca3d32cb15ffd0c23
sebastianbarreram <sbarrera.96@hotmail.com>
2022-10-12 16:45:06 -0500

    Intro JS

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+const persona = {
+    nombre: 'Tony',
+    apellido: 'Stark',
+    edad: 45,
+    direccion: {
+        ciudad: 'New York',
+        zip: 55321321,
+        lat: 14.3232,
+        lng: 34.9233321
+    }
+};
+
+console.table( persona ); 
+const activo = true;
+
+// let mensaje = '';
+
+// if ( !activo ) {
+//     mensaje = 'Activo';
+// } else {
+//     mensaje = 'Inactivo';
+// }
+// const mensaje = ( activo ) ? 'Activo' : 'Inactivo'; 
+// const mensaje = ( activo ) ? 'Activo' : null; 
+const mensaje = activo && 'Activo';
+
+
+console.log(mensaje);
+
+
